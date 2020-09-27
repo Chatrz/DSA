5242ad6b6b3f533101b93f888fe03cf2f44170b9
9831065 <9831065@ceit.local>
2020-09-27 14:18:06 +0330

    Data test

---
+++ Sortings/InsertionSort/Analysis/data_test.py
@@ -0,0 +1,77 @@
+from colorama import Fore   
+import random 
+import threading
+import datetime 
+import time
+from statistics import mean 
+
+START = 0
+STOP = 999999
+max_num = 100
+THREADS = 20
+compare_numbers = []
+
+def insertion_sort(numbers):
+    comparisons = 0
+
+    if(numbers):
+
+        for i in range(1,len(numbers)):
+            key=numbers[i]
+            j=i-1
+            
+            compare_j = 1
+
+            while(j>=0 and numbers[j]>=key):
+                numbers[j+1]=numbers[j]
+                j-=1
+                compare_j += 1
+
+            comparisons += compare_j
+
+            numbers[j+1]=key
+
+        return comparisons
+
+
+def numbers_generator():
+    global max_num
+    return [random.randint(START, STOP) for i in range(0, max_num)]
+
+def create_sorter(thread_id):
+    global compare_numbers
+    start_time = datetime.datetime.now()
+
+    numbers = numbers_generator()
+
+    print(Fore.YELLOW,end="")
+    print(f">> Sorter {thread_id + 1} start sorting {len(numbers)} numbers. Max = {max(numbers)} / Min = {min(numbers)}")
+    print(Fore.RESET,end="")
+
+    compare = insertion_sort(numbers) 
+    compare_numbers.append(compare)
+
+    finish_time = datetime.datetime.now()
+    total_time = (finish_time - start_time).total_seconds()
+
+    print(Fore.GREEN+"",end="")  
+    print(f"<< Sorter {thread_id + 1} is done. Time: {total_time} seconds / Comparisons: {compare}")
+    print(Fore.RESET,end="")
+
+
+if( __name__=='__main__'):
+    print(Fore.RED,end="")
+    print("\n>> Run\n")
+
+    for i in range(0, THREADS):
+        t = threading.Thread(target=create_sorter, args=(i,))
+        t.start()
+
+    print(Fore.RED,end="")
+    print("\n>> Done\n") 
+
+    time.sleep(5)
+    
+    print("\n")
+    print(Fore.YELLOW,end="")
+    print(f"Totally {THREADS} threads tested with average comparisons of {round(mean(compare_numbers), 2)}")
