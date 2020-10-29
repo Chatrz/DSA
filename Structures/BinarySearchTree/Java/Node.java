d507ec59a2c20bf5be7b26d6d29dc08ec0365887
sinamna <sina3050@gmail.com>
2020-10-29 11:05:56 +0330

    Node created

---
+++ Structures/BinarySearchTree/Java/Node.java
@@ -0,0 +1,12 @@
+public class Node{
+    int value;
+    Node right;
+    Node left;
+    // Node parent;
+    public Node(int value){
+        this.value=value;
+        right=null;
+        left=null;
+        // parent=null;
+    }
+}
