019bf29a37859288d9bef546236f61567f8f82c5
sinamna <sina3050@gmail.com>
2020-11-11 10:40:38 +0330

    added creating heap from unsorted array

--- Structures/BinaryHeap/Java/MaxHeap.java
+++ Structures/BinaryHeap/Java/MaxHeap.java
@@ -1,20 +1,32 @@
-public class MaxHeap{
+public class MaxHeap {
     private int[] heapArr;
-    private int size;
+    private int lastEl;
     public MaxHeap(){
         //creating a heap with maximum size 25
         heapArr=new int[25];
-        size=0;
+        lastEl=0;
     }
-
+    public MaxHeap(int[] heapArr){
+        this.heapArr=heapArr;
+        this.lastEl=heapArr.length-1;
+        buildMaxHeap();
+    }
     // create heap from unsorted array
+    public void buildMaxHeap(){
+        System.out.println(lastEl);
+        for (int i=(int)Math.floor((lastEl-1)/2);i>=0;i--){//watch out for the 0-index based
+            maxHeapify(i);
+        }
+    }
     //heapify
     private void maxHeapify(int index){
         int leftEl=index*2+1;//in 0-indexed array the left element is in 2*i+
         int rightEl=index*2+2;
         int maxEl=index;
-        if(leftEl<=size && heapArr[leftEl]>heapArr[index])maxEl=leftEl;
-        if(rightEl<=size && heapArr[rightEl]>heapArr[maxEl])maxEl=rightEl;
+        if(leftEl<=lastEl&& heapArr[leftEl]>heapArr[index])
+            maxEl=leftEl;
+        if(rightEl<=lastEl && heapArr[rightEl]>heapArr[maxEl])
+            maxEl=rightEl;
         if(maxEl!=index){
             int temp =heapArr[index];
             heapArr[index]=heapArr[maxEl];
@@ -28,5 +40,10 @@ public class MaxHeap{
     //extract max 
     //increase key;
     //print heap
-
+    //print array
+    public void printArr(){
+        for(int el:heapArr){
+            System.out.print(el+" ");
+        }
+    }
 }
