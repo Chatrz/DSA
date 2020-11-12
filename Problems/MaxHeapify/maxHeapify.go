eb3244cf9888c5cf56cf83303f874be57cb09d38
hadi <hawwwdi@gmail.com>
2020-11-12 12:56:20 +0330

    Go: maxHeapify

---
+++ Problems/MaxHeapify/maxHeapify.go
@@ -0,0 +1,27 @@
+package MaxHeapify
+
+func MaxHeapify(heap []int, root int) {
+	size := len(heap)
+	left := left(root)
+	right := right(root)
+	largest := root
+	if left < size && heap[left] > heap[root] {
+		largest = left
+	}
+	if right < size && heap[right] > heap[largest] {
+		largest = right
+	}
+	if largest != root {
+		heap[root], heap[largest] = heap[largest], heap[root]
+		MaxHeapify(heap, largest)
+	}
+
+}
+
+func left(i int) int {
+	return 2*i + 1
+}
+
+func right(i int) int {
+	return 2*i + 2
+}
---
+++ Problems/MaxHeapify/maxHeapify_test.go
@@ -0,0 +1,23 @@
+package MaxHeapify
+
+import "testing"
+
+func TestMaxHeapify(t *testing.T) {
+	tests := []struct {
+		given    []int
+		expected []int
+	}{
+		{
+			[]int{1, 2, 3, 4, 5},
+			[]int{3, 2, 1, 4, 5},
+		},
+	}
+	for _, test := range tests {
+		MaxHeapify(test.given, 0)
+		for k, v := range test.given {
+			if v != test.expected[k] {
+				t.Errorf("%v != %v\n", v, test.expected[k])
+			}
+		}
+	}
+}
