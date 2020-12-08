dde3e0ffda7704d9791a3b604d464127250f9bb6
ARMIN <armingodarzi1380@gmail.com>
2020-12-08 12:32:51 +0330

    changes in LinkedList ds and Search added for hashTable

--- Structures/HashTable/Go/chaining.go
+++ Structures/HashTable/Go/chaining.go
@@ -2,17 +2,17 @@ package main
 
 import (
 	"fmt"
-	list "github.com/sinamna/DSA/Structures/LinkedList/Go/armin"
 )
 
-type item struct {
+type Item struct {
 	key   int
 	value int
 }
 
+
 type HashTable struct {
 	size  int
-	array []list.LinkedList
+	array []LinkedList
 }
 
 func (table *HashTable) hashCode(key int) int {
@@ -22,40 +22,26 @@ func (table *HashTable) hashCode(key int) int {
 func NewHashTable(size int) *HashTable {
 	table := &HashTable{
 		size:  size,
-		array: make([]list.LinkedList, size),
+		array: make([]LinkedList, size),
 	}
 	return table
 }
 
-/*struct DataItem *search(int key) {
-   //get the hash
-   int hashIndex = hashCode(key);
-
-   //move in array until an empty
-   while(hashArray[hashIndex] != NULL) {
-
-      if(hashArray[hashIndex]->key == key)
-         return hashArray[hashIndex];
-
-      //go to next cell
-      ++hashIndex;
-
-      //wrap around the table
-      hashIndex %= SIZE;
-   }
-
-   return NULL;
-}*/
+func (table *HashTable) Search(key int) *Item {
+	//get the hash
+	hashIndex := table.hashCode(key)
+	node := table.array[hashIndex].Search(key)
+	return &node.Key
+}
 
 func (table *HashTable) Insert(key int, value int) {
-	item := item{}
+	item := Item{}
 	item.key = key
 	item.value = value
 	//get the hash
 	hashIndex := table.hashCode(key)
 	table.array[hashIndex].AddBack(item)
 }
-
 /*struct DataItem* delete(struct DataItem* item) {
    int key = item->key;
 
@@ -85,16 +71,24 @@ func (table *HashTable) Insert(key int, value int) {
 
 func (table *HashTable) Display() {
 	for i := 0; i < table.size; i++ {
-    fmt.Print("for slot ")
-    fmt.Print(i)
-    fmt.Println(" :")
-    table.array[i].Display()
-    fmt.Println("###############################################")
+		fmt.Print("for slot ")
+		fmt.Print(i)
+		fmt.Println(" :")
+		table.array[i].Display()
+		fmt.Println("###############################################")
 	}
 }
 
 func main() {
 	table := NewHashTable(10)
-	table.Insert(2, 3)
-  table.Display()
+	table.Insert(12, 3)
+	table.Insert(2, 4)
+	table.Insert(15, 5)
+	table.Insert(21, 17)
+	table.Insert(13, 9)
+	table.Insert(14, 1)
+	table.Insert(4, 31)
+	table.Display()
+  itemm := table.Search(12)
+	fmt.Println(itemm.value)
 }
---
+++ Structures/HashTable/Go/list.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Node struct {
+	Key  Item
+	Next *Node
+	Prev *Node
+}
+
+type LinkedList struct {
+	Size int
+	Head *Node
+	Tail *Node
+}
+
+func CreateLinkedList() *LinkedList {
+	list := LinkedList{
+		Size: 0,
+		Head: nil,
+		Tail: nil,
+	}
+	return &list
+}
+
+func (list *LinkedList) AddBack(key Item) {
+	if list.Size == 0 {
+		list.firstInitialize(key)
+	} else if list.Size == 1 {
+		node := &Node{
+			Key:  key,
+			Prev: nil,
+			Next: list.Head,
+		}
+		list.Head.Prev = node
+		list.Tail = list.Head
+		list.Head = node
+	} else {
+		node := &Node{
+			Key:  key,
+			Prev: nil,
+			Next: list.Head,
+		}
+		list.Head.Prev = node
+		list.Head = node
+	}
+	list.Size = list.Size + 1
+}
+
+// adding first element to linked list
+func (list *LinkedList) firstInitialize(key Item) {
+	node := &Node{
+		Key:  key,
+		Prev: nil,
+		Next: nil,
+	}
+	list.Head = node
+}
+
+func (list *LinkedList) Delete(index int) *Node {
+	node := list.Search(index)
+	if node == nil {
+		return nil
+	} else {
+		list.Size = list.Size - 1
+		if node.Next == nil { //node is tail of list
+			list.Tail = node.Prev
+			node.Prev.Next = nil
+		} else if node.Prev == nil { //nod is head of list
+			list.Head = node.Next
+			node.Next.Prev = nil
+		} else {
+			node.Next.Prev = node.Prev
+			node.Prev.Next = node.Next
+		}
+		return node
+	}
+}
+
+func (list *LinkedList) Display() {
+	rooter := list.Head
+	fmt.Print("list is :   ")
+	for rooter != nil {
+		fmt.Print(rooter.Key)
+		fmt.Print("  ")
+		rooter = rooter.Next
+	}
+	fmt.Println()
+}
+
+func (list *LinkedList) Search(index int) *Node {
+	node := list.Head
+	if node.Key.key == index {
+		return node
+	}
+	for node != list.Tail {
+		node = node.Next
+		if node.Key.key == index {
+			return node
+		}
+	}
+	/*if list.Tail.Key.key == index {
+		return list.Tail
+	}*/
+	return nil
+}
