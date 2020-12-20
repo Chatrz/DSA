0465def863e2e97f48d31867484ec3f0175923a9
ARMIN <armingodarzi1380@gmail.com>
2020-12-20 14:39:54 +0330

    dijkstra for directed graph added and tested

--- Graph/Armin/dijkstra.go
+++ Graph/Armin/dijkstra.go
@@ -5,6 +5,7 @@ import (
 )
 
 // undirected version inpired from == > https://www.youtube.com/watch?v=pVfj6mxhdMw
+// directed version needed changes came in line **45** of this implementation. 
 
 func (g *Graph) DijkstraUndirected(startingPoint *Vertex) (map[*Vertex]int, map[*Vertex]*Vertex) {
 	status := make(map[*Vertex]bool)        // true == > visited ,false == > unvisited
@@ -23,7 +24,16 @@ func (g *Graph) DijkstraUndirected(startingPoint *Vertex) (map[*Vertex]int, map[
 	g.PrintAdjacentList(g.AdjacenyList)
 
 	currentVertex := startingPoint
+
 	for currentVertex != nil {
+		//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		// If we are only interested in a shortest path between startingPoint and a particular vertex like V we can end the program here by adding :
+		/*
+		   if currentVertex == V {
+		     break
+		   }
+		*/
+		//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 		status[currentVertex] = true
 		currentEdge := g.AdjacenyList[currentVertex] // setting head of list as current edge
 		minDistance := 100000
@@ -32,7 +42,14 @@ func (g *Graph) DijkstraUndirected(startingPoint *Vertex) (map[*Vertex]int, map[
 		var neighbor *Vertex
 
 		for currentEdge != nil {
-
+			// if we wanted to do perform this algorithm to a directed graph the only change we had to make was
+			// putting this piece of code instead of line  53  to 57  :
+			/*if currentVertex == currentEdge.StartPoint {
+				neighbor = currentEdge.EndPoint
+			} else {
+				currentEdge = currentEdge.Next
+				continue
+			}*/
 			if currentVertex == currentEdge.EndPoint {
 				neighbor = currentEdge.StartPoint
 			} else {
@@ -44,6 +61,7 @@ func (g *Graph) DijkstraUndirected(startingPoint *Vertex) (map[*Vertex]int, map[
 				shortPath[neighbor] = distances
 				prevVertex[neighbor] = currentVertex
 			}
+
 			if !status[neighbor] {
 				if minDistance > currentEdge.Weight {
 					minDistance = currentEdge.Weight
@@ -53,6 +71,7 @@ func (g *Graph) DijkstraUndirected(startingPoint *Vertex) (map[*Vertex]int, map[
 
 			currentEdge = currentEdge.Next
 		}
+
 		currentVertex = closeNeighbor
 	}
 	return shortPath, prevVertex
@@ -74,23 +93,33 @@ func printDijsktraResult(path map[*Vertex]int, prev map[*Vertex]*Vertex) {
 	}
 }
 
+func PrintPath(startVertex, destVertex *Vertex, prev map[*Vertex]*Vertex) {
+	currentVertex := destVertex
+	fmt.Println(" PATH IS : (It is back ward version )")
+	for currentVertex != nil {
+		fmt.Print(currentVertex.Key)
+		fmt.Print(" < ==  ")
+		if currentVertex == startVertex {
+			break
+		}
+		currentVertex = prev[currentVertex]
+	}
+	fmt.Println()
+}
+
 func main() {
 	g := NewGraph(5)
-
 	for i := 0; i < 5; i++ { // 0 ==A , 1 == B , 2 == C , 3 == D , 4 == E
 		g.InsertVertex(i)
 	}
-
 	g.InsertEdge(0, 1, 6)
 	g.InsertEdge(0, 3, 1)
-
 	g.InsertEdge(1, 3, 2)
 	g.InsertEdge(1, 4, 2)
 	g.InsertEdge(1, 2, 5)
-
 	g.InsertEdge(3, 4, 1)
 	g.InsertEdge(4, 2, 5)
-
-	printDijsktraResult(g.DijkstraUndirected(g.Vertices[1]))
-
+	path, prev := g.DijkstraUndirected(g.Vertices[0])
+	printDijsktraResult(path, prev)
+	PrintPath(g.Vertices[0], g.Vertices[1], prev)
 }
