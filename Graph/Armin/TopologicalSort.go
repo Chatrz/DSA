c486c7afd02db208e768d68585e7feb7761d9b4a
ARMIN <armingodarzi1380@gmail.com>
2020-12-31 18:05:12 +0330

    check cyclic method added & tested

--- Graph/Armin/TopologicalSort.go
+++ Graph/Armin/TopologicalSort.go
@@ -1,6 +1,49 @@
 package main
 
-import ("fmt")
+import (
+	"fmt"
+)
+
+func (g *Graph) IsCyclic() bool {
+	// Mark all the vertices as not visited and not part of recursion
+	// stack
+	visited := make(map[*Vertex]bool)
+	recStack := make(map[*Vertex]bool)
+	for _, v := range g.Vertices {
+		visited[v] = false
+		recStack[v] = false
+	}
+	// Call the recursive helper function to detect cycle in different
+	// DFS trees
+	for _, v := range g.Vertices {
+		if g.isCyclicUtil(v, visited, recStack) {
+			return true
+		}
+	}
+	return false
+}
+
+func (g *Graph) isCyclicUtil(v *Vertex, visited map[*Vertex]bool, recStack map[*Vertex]bool) bool {
+	if !visited[v] {
+		// Mark the current node as visited and part of recursion stack
+		visited[v] = true
+		recStack[v] = true
+		// Recur for all the vertices adjacent to this vertex
+		temp := g.AdjacenyList[v]
+		for temp != nil {
+			if temp.StartPoint == v {
+				if !visited[temp.EndPoint] && g.isCyclicUtil(temp.EndPoint, visited, recStack) {
+					return true
+				} else if recStack[temp.EndPoint] {
+					return true
+				}
+			}
+			temp = temp.Next
+		}
+	}
+	recStack[v] = false // remove the vertex from recursion stack
+	return false
+}
 
 func (g *Graph) TopologicalSort(v *Vertex, visited map[*Vertex]bool, stack *Stack) {
 	visited[v] = true
@@ -13,7 +56,7 @@ func (g *Graph) TopologicalSort(v *Vertex, visited map[*Vertex]bool, stack *Stac
 				g.TopologicalSort(temp.EndPoint, visited, stack)
 			}
 		}
-    temp = temp.Next
+		temp = temp.Next
 	}
 	// Push current vertex to stack
 	// which stores result
@@ -51,10 +94,13 @@ func main() {
 	g.InsertEdge(3, 1, 1)
 	g.AdjacenyList = g.GetAdjacencyList(false)
 	g.PrintAdjacentList(g.AdjacenyList)
-  list := g.GetTopList()
-  for !list.IsEmpty(){
-    fmt.Print(list.Pop().Key)
-    fmt.Print(" ")
-  }
-  fmt.Println()
+	if !g.IsCyclic() {
+		fmt.Println("tsssssssssssss")
+	}
+	list := g.GetTopList()
+	for !list.IsEmpty() {
+		fmt.Print(list.Pop().Key)
+		fmt.Print(" ")
+	}
+	fmt.Println()
 }
