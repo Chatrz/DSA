bb86b1120dbe9d88d7c44bc886dc0ff569f47560
sinamna <sina3050@gmail.com>
2021-01-02 22:44:17 +0330

    base stucture of graph

---
+++ Structures/Graph/CPP/graph.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Graph{
+    private:
+        int v;
+        vector <vector<int>> adjList;
+    public:
+        Graph(){
+
+        }
+        void add_edge(int u, int v){
+
+        }
+        void print_adj_list(){
+
+        }
+        void print_adj_matrix(){
+            
+        }
+};
